6c16f9fbe390014f33283f972c4785c228f6f0c6
quaziyadgar <quaziyadgar@github.com>
2022-05-06 14:21:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/onlinedictionaryapp/DbManager.java
@@ -0,0 +1,30 @@
+package com.example.onlinedictionaryapp;
+
+import android.content.Context;
+import android.os.StrictMode;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbManager
+{
+  public Connection getCon(Context context)
+  {
+      Connection con=null;
+      StrictMode.ThreadPolicy policy=new StrictMode.ThreadPolicy.Builder().permitAll().build(); //allows all external thread
+      StrictMode.setThreadPolicy(policy);
+      //load the driver
+      try {
+          Class.forName("com.mysql.jdbc.Driver");
+          con= DriverManager.getConnection("jdbc:mysql://p3nlmysql51plsk.secureserver.net:3306/mmmut","mmmut","mmmut@123");
+          Toast.makeText(context, "Connection is established", Toast.LENGTH_SHORT).show();
+      } catch (ClassNotFoundException e) {
+          e.printStackTrace();
+      } catch (SQLException throwables) {
+          throwables.printStackTrace();
+      }
+      return con;
+  }
+}
---
+++ app/src/main/java/com/example/onlinedictionaryapp/LoginActivity.java
@@ -0,0 +1,55 @@
+package com.example.onlinedictionaryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText et_userid;
+    EditText et_password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        et_userid=findViewById(R.id.et_userid);
+        et_password=findViewById(R.id.et_password);
+    }
+    public void login(View view)
+    {
+        if (et_userid.getText().toString().isEmpty())
+        {
+            et_userid.setError("Empty");
+            et_userid.requestFocus();
+        }
+        else
+        {
+            if(et_password.getText().toString().isEmpty())
+            {
+                et_password.setError("Empty");
+                et_password.requestFocus();
+            }
+            else
+            {
+                String userid=et_userid.getText().toString().trim();
+                String password=et_password.getText().toString().trim();
+                if (userid.equals("admin")&&password.equals("admin@1234"))
+                {
+                    Intent I=new Intent(LoginActivity.this,AddWordActivity.class);
+                    startActivity(I);
+                    finish();
+                }
+                else
+                {
+                    Toast.makeText(this,"invalid user",Toast.LENGTH_LONG).show();
+                    et_userid.setText("");
+                    et_password.setText("");
+                }
+            }
+        }
+    }
+}
